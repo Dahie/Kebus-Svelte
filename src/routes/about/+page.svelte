ad8aac76dab49d2b6da04c62e6f604907379d7cf
Daniel Senff <mail@danielsenff.de>
2023-02-12 09:07:57 +0100

    improve about text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,11 +8,14 @@
 	<section>
 		<h1>About</h1>
 
-		<p>You are travelling, you arrive in a new town. So how do you get around? Where do you get tickets for regional public transport?</p>
 		<p>
-			While many transport companies have introduced digital services, these are often fragmented and every organisation offers their own app.
-			This is where Kebus helps by showing you the available ticket apps in your area.
-
+			You are travelling, you arrive in a new town. 
+			So how do you get around? 
+			Where do you get tickets for regional public transport?
+		</p>
+		<p>
+			While many transport companies have introduced digital services for ticket purchases, these are often fragmented and every organisation offers their own app.
+			This is where Kebus helps. While it doesn't sell tickets, it helps you finding the available ticket apps in your area.
 		</p>
 		<p>Kebus is a work in progress. Germany alone has over 40 transport associations and to cover all will take time and help. If you find apps or regions missing, <a href="mailto:public+kebus@danielsenff.de">let me know</a> and I will add them.</p>
 	</section>
