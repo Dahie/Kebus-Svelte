fa37e810a99358745a643bf6dad28113eac3e7ba
Daniel Senff <mail@danielsenff.de>
2023-02-12 11:53:40 +0100

    show menu on mobile

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,3 +1,7 @@
+<script>
+  	let showMenu = false;
+</script>
+
 <nav>
   <div class="nav-container">
       <div class="nav-logo">
@@ -8,20 +12,42 @@
         <li><a href="/apps">Apps</a></li>
         <li><a href="/about">About</a></li>
       </ul>
-      <a class="mobile-menu-toggle"></a>
-      <ul class="mobile-menu menu">
-        <li><a href="/apps">Apps</a></li>
-        <li><a href="/about">About</a></li>
-      </ul>
+      <a class="mobile-menu-toggle" on:click={() => showMenu = true}></a>
+      {#if showMenu}
+        <ul class="mobile-menu menu">
+          <li><a href="/apps">Apps</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+        <div class="modal-background" on:click={() => showMenu = false}></div>
+      {/if}
   </div>
+  
 </nav>
 
 <style>
   nav {
     background-color: var(--dark-grey-bg-color);
   }
-  nav a {
+
+  nav .nav-links a {
     color: white
   }
+
+  .menu.mobile-menu {
+    display: block;
+  }
+
+  .mobile-menu-toggle::before {
+    background-color: white;
+    box-shadow: 0 8px 0 0 white,0 16px 0 0 white;
+  }
+
+  .modal-background {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
   
 </style>
