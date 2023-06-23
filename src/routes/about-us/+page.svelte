2ae0a728026698183b6417bb436062f5847f5b44
Daniel Senff <mail@danielsenff.de>
2023-06-23 07:54:04 +0200

    adding meta tags for subpages, some wording changes

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -3,21 +3,32 @@
   import Footer from '../../Footer.svelte'
 </script>
 
+<svelte:head>
+    <title>About - Kebus</title> 
+		<meta name="description" content="How Kebus came to be, its purpose and how to contribute.">
+</svelte:head>
+
+
 <Navbar />
 <main class="container">
 	<section>
 		<h1>About</h1>
 
 		<p>
-			You are travelling, you arrive in a new town. 
-			So how do you get around? Where do you find city buses?
+			You are travelling, you arrive in a new town. <br>
+			So how do you get around? 
+			Where do you find local buses?
 			Where do you get tickets for regional public transport?
 		</p>
 		<p>
 			While many transport companies have introduced digital services for ticket purchases, these are often fragmented and every organisation offers their own app.
 			This is where Kebus helps. While it doesn't sell tickets, it helps you finding the available ticket apps in your area.
 		</p>
-		<p>Kebus is a work in progress. Germany alone has over 40 transport associations and to cover all will take time and help. If you find apps or regions missing, <a href="mailto:public+kebus@danielsenff.de">let me know</a> and I will add them.</p>
+		<p>
+			Kebus is a work in progress. Germany alone has over 40 transport associations and to cover all will take time and help. 
+			If you know missing apps in your area, we are happy to 
+			<a href="https://docs.google.com/forms/d/e/1FAIpQLSdHke4T1FK0I2_ICZaqHIskhmwuP17M7DEMAZqPlvX6AzhETg/viewform?usp=sf_link" target="_blank" rel="noreferrer">add new submissions</a>.
+		</p>
 	</section>
 </main>
 <Footer />
--- src/routes/apps/+page.svelte
+++ src/routes/apps/+page.svelte
@@ -4,6 +4,10 @@
   import Footer from '../../Footer.svelte'
 </script>
 
+<svelte:head>
+    <title>Apps - Kebus</title> 
+    <meta name="description" content="Kebus provides an index of Ticket Apps for regional and city public transport in its respective areas.">
+</svelte:head>
 
 <Navbar />
 <main class="container">
